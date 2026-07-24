ce8d5a373310deec2dfc24ccc4a59c1812dd8595
Sainosaur <saiadi4002@gmail.com>
2026-07-24 13:18:32 +0100

    First iteration of strobe_generator built. Ready for individual TDD

--- src/strobe_generator.vhd
+++ src/strobe_generator.vhd
@@ -4,11 +4,8 @@ USE ieee.numeric_std.all;
 
 
 ENTITY strobe_generator IS
-GENERIC (
-    strobe_width : integer := 1
-);
 PORT (
-    CLK: IN std_logic;
+    CLK: IN std_logic; -- Built assuming clock rate of 16MHz
     baud_rate: IN unsigned(2 DOWNTO 0);
     enable_tx: OUT std_logic;
     enable_rx: OUT std_logic
@@ -17,13 +14,26 @@ END strobe_generator;
 
 
 ARCHITECTURE behavioral OF strobe_generator IS
-SIGNAL rx_counter : unsigned(7 DOWNTO 0) := (OTHERS => '0');
-SIGNAL tx_counter: unsigned(3 DOWNTO 0) := (OTHERS => '0');
+    TYPE baud_rate_array IS ARRAY (0 TO 7) OF unsigned (7 DOWNTO 0) ;
+    SIGNAL tx_counter : unsigned(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL rx_counter: unsigned(3 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL baud_rate_counters: baud_rate_array := ("00110100", "00011010", "00001101", "00000110", "00000011", "00000000", "00000000", "00000000");
 BEGIN
     PROCESS (clk)
     BEGIN
         IF rising_edge(clk) THEN
-            IF rx_counter <
+            IF tx_counter < baud_rate_counters(baud_rate) THEN
+                enable_tx <= '0';
+                tx_counter <= tx_counter + 1;
+            ELSE
+                enable_tx <= '1';
+                tx_counter <= '0';
+                rx_counter <= rx_counter + 1;
+            END IF;
+            IF rx_counter = X"F" THEN
+                enable_rx <= '1';
+                rx_counter <= (OTHERS => '0');
+            END IF;
         END IF;
     END PROCESS;
 END behavioral;
