10926acc1f90a0a80570747c7173a44c04f7e099
Sainosaur <saiadi4002@gmail.com>
2026-07-24 13:18:54 +0100

    Update strobe_generator.vhd

--- src/strobe_generator.vhd
+++ src/strobe_generator.vhd
@@ -15,24 +15,26 @@ END strobe_generator;
 
 ARCHITECTURE behavioral OF strobe_generator IS
     TYPE baud_rate_array IS ARRAY (0 TO 7) OF unsigned (7 DOWNTO 0) ;
-    SIGNAL tx_counter : unsigned(7 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL rx_counter: unsigned(3 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL baud_rate_counters: baud_rate_array := ("00110100", "00011010", "00001101", "00000110", "00000011", "00000000", "00000000", "00000000");
+    SIGNAL rx_counter : unsigned(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL tx_counter: unsigned(3 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL baud_rate_counters: baud_rate_array := ("01101000", "00110100", "00011010", "00001100", "00000110", "00000000", "00000000", "00000000");
 BEGIN
     PROCESS (clk)
     BEGIN
         IF rising_edge(clk) THEN
-            IF tx_counter < baud_rate_counters(baud_rate) THEN
-                enable_tx <= '0';
-                tx_counter <= tx_counter + 1;
-            ELSE
-                enable_tx <= '1';
-                tx_counter <= '0';
+            IF rx_counter < baud_rate_counters(baud_rate) THEN
+                enable_rx <= '0';
                 rx_counter <= rx_counter + 1;
-            END IF;
-            IF rx_counter = X"F" THEN
+            ELSE
                 enable_rx <= '1';
-                rx_counter <= (OTHERS => '0');
+                rx_counter <= '0';
+                tx_counter <= tx_counter + 1;
+            END IF;
+            IF tx_counter = X"F" THEN
+                enable_tx <= '1';
+                tx_counter <= (OTHERS => '0');
+            ELSE
+                enable_tx <= '0';
             END IF;
         END IF;
     END PROCESS;
